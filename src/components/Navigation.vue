209dd8c27ce23ad6699cec769eb49fa57780be12
KocKaan <kk4291@nyu.edu>
2021-11-15 04:15:02 -0500

    Minor fix of router-link and css addition

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,6 +1,6 @@
 <template>
 <header>
-    <nav class= "containe"> 
+    <nav class= "container"> 
         <div class="branding">
             <router-link class="header" :to="{ name: 'Home'}">
                 CapitalBlog
@@ -8,23 +8,23 @@
         </div>
         <div class="nav-links">
             <ul>
-                <router-links class= "links" to="#">Home</router-links>
-                <router-links class= "links" to="#">Blogs</router-links>
-                <router-links class= "links" to="#">Create Post</router-links>
-                <router-links class= "links" to="#">Login/Register</router-links> 
+                <router-link class= "link" to="#">Home</router-link>
+                <router-link class= "link" to="#">Blogs</router-link>
+                <router-link class= "link" to="#">Create Post</router-link>
+                <router-link class= "link" to="#">Login/Register</router-link> 
             </ul>
         </div>
     </nav>
-    <menuIcon/>
+    <menuIcon class="menu-icon"/>
     <transition name= "mobile-nav">
             <ul>
-                <router-links class= "links" to="#">Home</router-links>
-                <router-links class= "links" to="#">Blogs</router-links>
-                <router-links class= "links" to="#">Create Post</router-links>
-                <router-links class= "links" to="#">Login/Register</router-links> 
+                <router-link class= "link" to="#">Home</router-link>
+                <router-link class= "link" to="#">Blogs</router-link>
+                <router-link class= "link" to="#">Create Post</router-link>
+                <router-link class= "link" to="#">Login/Register</router-link> 
             </ul>
     </transition>
-    
+   
 </header>    
 </template>
 
@@ -40,4 +40,59 @@ export default {
 </script>
 
 
-<style lang="scss" scoped> </style>
+<style lang="scss" scoped> 
+header{
+    background-color: #fff;
+    padding: 0 25;
+    box-shadow: 0 4px 6px -1px rgba(0,0,0,0), 0 2px 4px -1px rgba(0,0,0,0.06);
+    z-index: 99;
+}
+.link{
+    font-weight: 500;
+    padding: 0 8px;
+    transition: .3s color ease;
+
+    &:hover{
+        color: #1eb8b8;
+    }
+}
+nav{
+    display: flex;
+    padding: 25px 0;
+
+    .branding{
+        display: flex;
+        align-items: center;
+
+        .header{
+            font-weight: 600;
+            font-size: 24px;
+            color: #000;
+            text-decoration: none;
+        }
+    }
+
+    .nav-links{
+        position: relative;
+        display: flex;
+        flex: 1;
+        align-items: center;
+        justify-content: flex-end;
+    }
+
+    .ul{
+        margin-right: 32px;
+
+        .link{
+            margin-right: 32;
+        }
+        
+        .link:last-child{
+            margin-right: 0;
+        }
+    }
+      
+
+}
+
+</style>
