c8fa8a0598c257a7241425194a700bdc741b3925
KocKaan <kk4291@nyu.edu>
2021-11-14 23:55:11 -0500

    Installed firabase config

---
+++ src/firebase/firebaseInit.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAArQp2Bt9VMQcrRh4bGtR2SyB_HVKJUhc",
+  authDomain: "capitalblog-8d6a7.firebaseapp.com",
+  projectId: "capitalblog-8d6a7",
+  storageBucket: "capitalblog-8d6a7.appspot.com",
+  messagingSenderId: "610176721913",
+  appId: "1:610176721913:web:a7037fa45daa8ab1bdd328"
+};
