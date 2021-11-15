c74074e0dd5ccf02a5b1f234eb5436f2bee0c9f6
KocKaan <kk4291@nyu.edu>
2021-11-15 00:26:51 -0500

    Created the connection between vue to firebase

--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -1,3 +1,6 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+ 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAArQp2Bt9VMQcrRh4bGtR2SyB_HVKJUhc",
@@ -7,3 +10,12 @@ const firebaseConfig = {
   messagingSenderId: "610176721913",
   appId: "1:610176721913:web:a7037fa45daa8ab1bdd328"
 };
+
+
+const firebaseApp= firebase.initializeApp(firebaseConfig); 
+const timestamp= firebase.firestore.FieldValue.serverTimestamp;
+
+//front end backend connection to firebase 
+export{timestamp};
+export default firebaseApp.firestore();
+ 
