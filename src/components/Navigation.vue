7b572b23e12bffb42a65bce1ceaff37d733d69d5
KocKaan <kk4291@nyu.edu>
2021-11-15 02:18:04 -0500

    Created the navigation container

---
+++ src/components/Navigation.vue
@@ -0,0 +1,43 @@
+<template>
+<header>
+    <nav class= "containe"> 
+        <div class="branding">
+            <router-link class="header" :to="{ name: 'Home'}">
+                CapitalBlog
+            </router-link>
+        </div>
+        <div class="nav-links">
+            <ul>
+                <router-links class= "links" to="#">Home</router-links>
+                <router-links class= "links" to="#">Blogs</router-links>
+                <router-links class= "links" to="#">Create Post</router-links>
+                <router-links class= "links" to="#">Login/Register</router-links> 
+            </ul>
+        </div>
+    </nav>
+    <menuIcon/>
+    <transition name= "mobile-nav">
+            <ul>
+                <router-links class= "links" to="#">Home</router-links>
+                <router-links class= "links" to="#">Blogs</router-links>
+                <router-links class= "links" to="#">Create Post</router-links>
+                <router-links class= "links" to="#">Login/Register</router-links> 
+            </ul>
+    </transition>
+    
+</header>    
+</template>
+
+<script>
+import menuIcon from '../assets/Icons/bars-regular.svg';
+export default {
+    name: 'navigation',
+    components:{
+        menuIcon
+    }
+
+}
+</script>
+
+
+<style lang="scss" scoped> </style>
