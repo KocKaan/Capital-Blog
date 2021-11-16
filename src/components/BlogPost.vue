a4814452efc9712dae43b597433abca242d187b1
KocKaan <kk4291@nyu.edu>
2021-11-15 23:41:19 -0500

    Wrote the html part of blogPost

--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -1,12 +1,42 @@
 <template>
-  <div>
-    <h2>Hellooo</h2>
+  <div class="blog-wrapper">
+    <div class="blog-content">
+      <div>
+        <h2 v-if="post.welcomeScreen">{{ post.title }}</h2>
+        <h2 v-else>{{ post.title }}</h2>
+        <p v-if="post.welcomeScreen">{{ post.blogPost }}</p>
+        <p class="content-preview" v-else>{{ post.blogHTML }}</p>
+        <router-link class="link link-light" v-if="post.welcomeScreen" to="#">
+          Login/Register <Arrow class="arrow arrow-light" />
+        </router-link>
+
+        <router-link v-else to="#">
+          View the Post <Arrow class="arrow" />
+        </router-link>
+      </div>
+      <div class="blog-photo">
+        <img
+          v-if="post.welcomeScreen"
+          :src="require(`../assets/blogPhotos/${post.photo}.jpg`)"
+        />
+        <img
+          v-else
+          :src="require(`../assets/blogPhotos/${post.blogCoverPhoto}.jpg`)"
+          alt=""
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Arrow from "../assets/Icons/arrow-right-light.svg";
 export default {
   name: "blogPost",
+  props: ["post"],
+  components: {
+    Arrow,
+  },
 };
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="home">
-    <BlogPost />
-    <BlogPost />
+    <BlogPost :post="welcomeScreen" />
+    <BlogPost
+      :post="post"
+      v-for="(post, index) in sampleBlogPost"
+      :key="index"
+    />
   </div>
 </template>
 
