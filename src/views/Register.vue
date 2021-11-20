e4785d3f8ca542175b46211cdcf2118a72821d6f
KocKaan <kk4291@nyu.edu>
2021-11-20 01:24:39 -0500

    register view created

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,10 +1,70 @@
 <template>
-  <div>dwadawaw</div>
+  <div class="form-wrap">
+    <form class="register">
+      <p class="login-register">
+        Already have Account?
+        <router-link router="router-link" :to="{ name: `Register` }"
+          >Register</router-link
+        >
+      </p>
+      <h2>Create your Capital Blog Account</h2>
+      <div class="inputs">
+        <div class="input">
+          <input type="text" placeholder="First Name" v-model="firstName" />
+          <user class="icon" />
+        </div>
+        <div class="input">
+          <input type="text" placeholder="Last Name" v-model="lastName" />
+          <user class="icon" />
+        </div>
+        <div class="input">
+          <input type="text" placeholder="Username" v-model="username" />
+          <user class="icon" />
+        </div>
+        <div class="input">
+          <input type="text" placeholder="Email" v-model="email" />
+          <email class="icon" />
+        </div>
+        <div class="input">
+          <input type="password" placeholder="Password" v-model="password" />
+          <password class="icon" />
+        </div>
+      </div>
+      <button>Sign Up</button>
+
+      <div class="angle"></div>
+    </form>
+    <div class="background"></div>
+  </div>
 </template>
 
 <script>
-export default {};
+import email from "../assets/Icons/envelope-regular.svg";
+import password from "../assets/Icons/lock-alt-solid.svg";
+import user from "../assets/Icons/user-alt-light.svg";
+export default {
+  name: "Register",
+  components: {
+    email,
+    password,
+    user,
+  },
+  data() {
+    return {
+      firstName: null,
+      lastName: null,
+      username: null,
+      email: null,
+      password: null,
+    };
+  },
+};
 </script>
 
-<style>
+<style lang="scss" scoped>
+.register {
+  h2 {
+    max-width: 350px;
+  }
+}
 </style>
