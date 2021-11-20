f68633142c74221b0f7cb06b5a1e699d09ce08bf
KocKaan <kk4291@nyu.edu>
2021-11-20 02:20:57 -0500

    Added the firebase connection

--- src/App.vue
+++ src/App.vue
@@ -135,6 +135,12 @@ button,
   background-color: rgba(128, 128, 128, 0.5);
 }
 
+.error {
+  text-align: center;
+  font-size: 12px;
+  color: red;
+}
+
 .blog-card-wrap {
   position: relative;
   padding: 80px 16px;
