af1579280c10a4c0e66295d88332d23c8f0c0711
KocKaan <kk4291@nyu.edu>
2021-11-20 02:21:11 -0500

    Added the firebase connection to register

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -2,12 +2,12 @@
   <div class="form-wrap">
     <form class="register">
       <p class="login-register">
-        Already have Account?
-        <router-link router="router-link" :to="{ name: `Login` }"
+        Already have an account?
+        <router-link class="router-link" :to="{ name: 'Login' }"
           >Login</router-link
         >
       </p>
-      <h2>Create your Capital Blog Account</h2>
+      <h2>Create your FireBlog Account</h2>
       <div class="inputs">
         <div class="input">
           <input type="text" placeholder="First Name" v-model="firstName" />
@@ -29,9 +29,9 @@
           <input type="password" placeholder="Password" v-model="password" />
           <password class="icon" />
         </div>
+        <div v-show="error" class="error">{{ this.errorMsg }}</div>
       </div>
-      <button>Sign Up</button>
-
+      <button @click.prevent="register">Sign Up</button>
       <div class="angle"></div>
     </form>
     <div class="background"></div>
@@ -42,6 +42,9 @@
 import email from "../assets/Icons/envelope-regular.svg";
 import password from "../assets/Icons/lock-alt-solid.svg";
 import user from "../assets/Icons/user-alt-light.svg";
+import firebase from "firebase/app";
+import "firebase/auth";
+import db from "../firebase/firebaseInit";
 export default {
   name: "Register",
   components: {
@@ -51,15 +54,50 @@ export default {
   },
   data() {
     return {
-      firstName: null,
-      lastName: null,
-      username: null,
-      email: null,
-      password: null,
+      firstName: "",
+      lastName: "",
+      username: "",
+      email: "",
+      password: "",
+      error: null,
+      errorMsg: "",
     };
+  },
+  methods: {
+    async register() {
+      if (
+        this.email != "" &&
+        this.password != "" &&
+        this.firstName != "" &&
+        this.lastName != "" &&
+        this.username != ""
+      ) {
+        this.error = false;
+        this.errorMsg = "";
+        const firebaseAuth = await firebase.auth();
+        const createUser = await firebaseAuth.createUserWithEmailAndPassword(
+          this.email,
+          this.password
+        );
+        const result = await createUser;
+        const dataBase = db.collection("users").doc(result.user.uid);
+        await dataBase.set({
+          firstName: this.firstName,
+          lastName: this.lastName,
+          username: this.username,
+          email: this.email,
+        });
+        this.$router.push({ name: "Home" });
+        return;
+      }
+      this.error = true;
+      this.errorMsg = "Please fill out all the fields!";
+      return;
+    },
   },
 };
 </script>
+
 
 <style lang="scss" scoped>
 .register {
