0c1936344b73c9fbc5545041a01381dc3be75e9d
KocKaan <kk4291@nyu.edu>
2021-11-21 17:45:24 -0500

    You can add images to the text

--- src/views/CreatePost.vue
+++ src/views/CreatePost.vue
@@ -31,6 +31,7 @@
           :editorOptions="editorSettings"
           v-model="blogHTML"
           useCustomImageHandler
+          @image-added="imageHandler"
         />
       </div>
       <div class="blog-actions">
@@ -45,6 +46,8 @@
 import BlogCoverPreview from "../components/BlogCoverPreview";
 import Quill from "quill";
 window.Quill = Quill;
+import firebase from "firebase/app";
+import "firebase/storage";
 const ImageResize = require("quill-image-resize-module").default;
 Quill.register("modules/imageResize", ImageResize);
 export default {
@@ -74,6 +77,24 @@ export default {
     },
     openPreview() {
       this.$store.commit("openPhotoPreview");
+    },
+    imageHandler(file, Editor, cursorLocation, resetUploader) {
+      const storageRef = firebase.storage().ref();
+      const docRef = storageRef.child(`documents/blogPostPhotos/${file.name}`);
+      docRef.put(file).on(
+        "state_changed",
+        (snapshot) => {
+          console.log(snapshot);
+        },
+        (err) => {
+          console.log(err);
+        },
+        async () => {
+          const downloadURL = await docRef.getDownloadURL();
+          Editor.insertEmbed(cursorLocation, "image", downloadURL);
+          resetUploader();
+        }
+      );
     },
   },
   computed: {
